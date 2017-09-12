fc1100bb89b516b7e4e28e918a68e68f01af748b
msatz <sathishmsdr@gmail.com>
2017-09-12 22:42:03 +0530

    one file committed (increase in array)

--- Inc_button_value/ViewController.swift
+++ Inc_button_value/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet var butt2: UIButton!
     @IBOutlet var butt1: UIButton!
     @IBOutlet var butt3: UIButton!
-    var a=[0,1,2,3,4,5,6,7,8]
+    var a=[0,1,2,3,4,5,6,7,8,9]
     
   var i=0
     
